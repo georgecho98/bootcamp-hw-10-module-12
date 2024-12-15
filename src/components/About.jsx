8c1701dad8edcd6747329d308d1b7eee873c8468
hon chau <chaugeorge9@gmail.com>
2024-12-15 04:26:48 -0600

    final

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,17 @@
 import './style.css'
 
+const styles = {
+  AboutStyle: {
+   justifyContent: 'left',
+  }
+};
 function About() {
   return (
     <div>
-            <h1> About Me </h1>
-            <img src= './assets/images/fff.jpeg'></img>
+            <h1 style = {styles.AboutStyle}> ABOUT ME </h1>
+            
+            <div className="Mypic" ></div>
+            
             <p>
                   My name is Hon chau and I had graduated with a electric power engineering degree 16 years ago. 
                   I worked on manufacturing ontrol logics and whiskey brewery control design. 
